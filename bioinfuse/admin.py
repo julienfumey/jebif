0858429788fb1f973062a0dfa6eb88026f7d39e4
Norore <nolwenn.lavielle@gmail.com>
2016-04-25 08:49:40 +0200

    Increase Challenge database

--- bioinfuse/admin.py
+++ bioinfuse/admin.py
@@ -7,8 +7,11 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('user', 'show_name', 'role')
 
 class ChallengeAdmin(admin.ModelAdmin):
-    list_filter = ('title', 'is_open', 'start_date', 'stop_date')
-    list_display = ('title', 'is_open', 'start_date')
+    list_filter = ('title', 'is_open', 'start_date', 'subs_start_date',
+                   'subm_start_date')
+    list_display = ('title', 'is_open', 'start_date', 'stop_date',
+                    'subs_start_date', 'subs_stop_date', 'subm_start_date',
+                    'subm_stop_date')
 
 class AssociatedKeyAdmin(admin.ModelAdmin):
     list_filter = ('challenge', 'candidate')

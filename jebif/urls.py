6f508084c7b28d4ddddcad01846564d790fbe5ec
Norore <nolwenn.lavielle@gmail.com>
2016-04-28 23:07:03 +0200

    Correctly include bioinfuse urls

--- jebif/urls.py
+++ jebif/urls.py
@@ -12,14 +12,8 @@ BROOT = "%sbioinfuse/" % ROOT
 urlpatterns = [
     # Administration
     url(r'^%sadmin/' % ROOT, include(admin.site.urls)),
-    # BioInfuse
-    url(r'^%s' % BROOT, include('bioinfuse.urls')),
     # Member
     url(r'^%slogin/$' % ROOT, auth_views.login, name="login"),
     url(r'^%saccounts/logout/$' % ROOT, auth_views.logout, {'next_page': '/%s'%BROOT}, name="logout"),
-    url(r'^%ssubscribe/' % BROOT, include('bioinfuse.urls', namespace='subscribe')),
-    url(r'^%sedit_profile/(?P<member>[0-9]+)' % BROOT, include('bioinfuse.urls', namespace='edit_profile')),
-    url(r'^%smanage_members' % BROOT, include('bioinfuse.urls', namespace="manage_members")),
-    url(r'^%sedit_member/(?P<member>[0-9]+)' % BROOT, include('bioinfuse.urls', namespace="edit_member")),
-    url(r'^%ssubmit_movie/(?P<member>[0-9]+)' % BROOT, include('bioinfuse.urls', namespace="submit_movie")),
+    url(r'%sbioinfuse/' % ROOT, include('bioinfuse.urls')),
 ]
